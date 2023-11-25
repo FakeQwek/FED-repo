e43e81cf1b102725d1227bca758b67f42bd825eb
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2023-11-26 00:25:10 +0800

    Website Development Update
    - Added FAQ Page
    - Added Calculator Browsing Page
      1. Page has functional search bar

---
+++ FED_S10255665C_QwekTsunYang_website/scripts/script.js
@@ -0,0 +1,24 @@
+/* Search bar function taught by https://www.youtube.com/watch?v=ZFUOC-y4i0s*/
+
+
+const search = () =>{
+    const searchbox = document.getElementById("search").value.toUpperCase();
+    const storeCalc = document.getElementById("calculator-slots");
+    const calculator = document.querySelectorAll(".slot");
+    const calculatorName = storeCalc.getElementsByClassName("name");
+
+    for(var i = 0; i< calculatorName.length; i++) {
+        let match = calculator[i].getElementsByClassName("name")[0];
+        if(match) {
+            let text = match.textContent || match.innerHTML;
+            if (text.toUpperCase().indexOf(searchbox) > -1){
+                calculator[i].style.display = "";
+            } 
+            else {
+                calculator[i].style.display = "none";
+            }
+            
+            
+        }
+    }
+}
