046a2b8c70945f3c7538a4b9a07d27b1221aadcd
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2023-11-27 21:22:23 +0800

    Website Development Update
    - Added comments to CSS, HTML and Javascript files 
    - Added alt tags to images

--- FED_S10255665C_QwekTsunYang_website/scripts/script.js
+++ FED_S10255665C_QwekTsunYang_website/scripts/script.js
@@ -3,14 +3,18 @@
 /* Start of Searchbar */
 const search = () =>{
     const searchbox = document.getElementById("search").value.toUpperCase();
+    /* sets to upper so search is not case sensitive */ 
     const storeCalc = document.getElementById("calculator-slots");
     const calculator = document.querySelectorAll(".slot");
     const calculatorName = storeCalc.getElementsByClassName("name");
-
+    /* declaring html elements as variables */
     for(var i = 0; i< calculatorName.length; i++) {
+        /* set element with class name in slot */
         let match = calculator[i].getElementsByClassName("name")[0];
         if(match) {
+            
             let text = match.textContent || match.innerHTML;
+            /* checks each character at a time */
             if (text.toUpperCase().indexOf(searchbox) > -1){
                 calculator[i].style.display = "";
             } 
@@ -18,7 +22,8 @@ const search = () =>{
                 calculator[i].style.display = "none";
             }
             
-            
+            /* goes through each element with class slot matches, the text in search then 
+            hides the elements accordingly */
         }
     }
 }
